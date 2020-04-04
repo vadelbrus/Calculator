6dabb513576cba84d10c82261ccc618fee8baced
vadelbrus <vadelbrus@gmail.com>
2020-04-04 17:58:56 +0200

    Added some logic
    numPressed(num) allows to insert numbers combined with many digits
    
    operationPressed(operator) flag that checks if user wants to make an operation and type second number

--- Frontend/calc.js
+++ Frontend/calc.js
@@ -1,1 +1,26 @@
-<--- JS -->
+
+let firstValue = "";
+let secondValue = "";
+let result = "";
+let operatorFlag = false;
+
+
+function numPressed(num) {
+
+    if (!operatorFlag) {
+        firstValue += num;
+        document.querySelector('.display').value = firstValue;
+    } else {
+        secondValue += num;
+        document.querySelector('.display').value = secondValue;
+    }
+}
+
+function operationPressed(operator) {
+    operatorFlag = true;
+    document.querySelector('.display').value = "";
+    firstValue = "";
+    secondValue = "";
+}
+
+
