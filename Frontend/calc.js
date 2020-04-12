ab069b1d3757225150816c230fa883e2afbe9bbd
vadelbrus <vadelbrus@gmail.com>
2020-04-12 16:46:37 +0200

    next changes
    adding functionality in updateDisplay() function, getting rid of trailing zeros

--- Frontend/calc.js
+++ Frontend/calc.js
@@ -1,20 +1,18 @@
-
 // SETTING VARIABLES
 
 let firstValue = "";
 let secondValue = "";
 let result = "";
-// let operatorFlag = false;
 let isDecimal = false;
-// let isActive = true;
 let mathOperator = "";
+let subText = document.querySelector(".showOperation");
 
 // CHECKING WHETHER USER IS TYPING FIRST OR SECOND NUMBER 
 
 function numPressed(num) {
     if (mathOperator != "") {
 
-        // CHECKING IS DECIMAL IS CLICKED AND PREVENTING ITS DOUBLING
+        // CHECKING IF DECIMAL IS CLICKED AND PREVENTING ITS DOUBLING
 
         if (num === ".") {
             if (!isDecimal) {
@@ -23,7 +21,7 @@ function numPressed(num) {
             }
         }
         else secondValue += num;
-        document.querySelector('.display').value = secondValue;
+        updateDisplay(secondValue);
 
     } else {
 
@@ -34,49 +32,40 @@ function numPressed(num) {
                 firstValue += num;
                 isDecimal = true;
             }
-
         }
         else firstValue += num;
-        document.querySelector('.display').value = firstValue
-
+        updateDisplay(firstValue);
     }
 }
 
 // SETTING DEL KEY THAT REMOVES LAST DIGIT FROM NUMBER
 
 function numPressedDelete() {
-
     let delBuffer = "";
-
     if (mathOperator == "") {
         firstValue = firstValue.slice(0, -1);
         delBuffer = firstValue;
-
     }
     else {
         secondValue = secondValue.slice(0, -1);
         delBuffer = secondValue;
-
     }
-
     updateDisplay(delBuffer);
-
-
-}
-
-function updateDisplay(displayValue) {
-    document.querySelector('.display').value = displayValue;
 }
 
 // CHECKING IF USER CLICKS MATH OPERATOR
 
 function operationPressed(operator) {
-    // operatorFlag = !operatorFlag;
     mathOperator = operator;
-    document.querySelector('.display').value = operator;
-    // isActive = !isActive;
+    updateDisplay(operator);
     isDecimal = false;
+}
 
+// SETTING LIVE DISPLAY FUNCTION
+
+function updateDisplay(displayValue) {
+    document.querySelector('.display').value = displayValue;
+    subText.textContent = `${firstValue} ${mathOperator} ${secondValue} =`;
 }
 
 // MAKING CALCULATIONS DEPENDING ON CHOSEN OPERATOR
@@ -84,32 +73,32 @@ function operationPressed(operator) {
 function numPressedResult() {
 
     // CHANGING STRINGS INTO NUMBERS SO MATH OPERATIONS CAN BE POSSIBLE
-
+    
     firstValue = Number(firstValue);
     secondValue = Number(secondValue);
 
     // SOLVING DECIMAL PRECISION PROBLEM ADDING SOME MATH FUNCTIONS
     switch (mathOperator) {
         case "+":
-            // result = Math.round(parseFloat(((firstValue + secondValue) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
             result = firstValue + secondValue;
 
             break;
         case "-":
-            // result = Math.round(parseFloat(((firstValue - secondValue) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
+
             result = firstValue - secondValue;
             break;
         case "x":
-            // result = Math.round(parseFloat(((firstValue * secondValue) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
-            result = (firstValue * secondValue).toFixed(9).toString();
 
+            result = +(firstValue*secondValue).toFixed(9);
             break;
         case "/":
-            result = (firstValue / secondValue).toFixed(9).toString();
+            result = +(firstValue / secondValue).toFixed(9);
             break;
+
         case "%":
-            result = Math.round(parseFloat(((firstValue * (secondValue / 100)) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
+            result = +(firstValue * (secondValue / 100)).toFixed(9);
             break;
+            
         default:
             result = secondValue;
 
@@ -117,20 +106,12 @@ function numPressedResult() {
 
     // RESETING VARIABLES
 
-    document.querySelector('.display').value = result;
-    // isDecimal = false;
-    // firstValue = "";
-    // secondValue = "";
-
+    updateDisplay(result);
     firstValue = "";
     secondValue = "";
-    // operatorFlag = false;
     mathOperator = "";
-    // isActive = true;
     isDecimal = false;
-    // result = "";
-
-}
+    }
 
 // RESETING VARIABLES IF 'C' IS CLICKED
 
@@ -139,9 +120,8 @@ function numPressedClear() {
     secondValue = "";
     result = "";
     mathOperator = "";
-    // operatorFlag = false;
-    // isActive = true;
     isDecimal = false;
-    document.querySelector('.display').value = "0";
+    updateDisplay("0");
+    subText.textContent = "0";
 }
 
